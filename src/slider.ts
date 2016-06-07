226025348321aaf4ffbbff5fab417aa9e34de698
Steph Monette <steph@stephmonette.com>
2016-06-07 15:08:52 -0700

    adding chart css, trying to fix the slider

--- src/slider.ts
+++ src/slider.ts
@@ -9,34 +9,33 @@ declare var jQuery:any;
 
 export class Slider implements AfterViewInit {
         
-    @Input() slideValue: number;    
-    elementRef: ElementRef;
-    visibility = 'hidden';
-    @Input() index: number;
+    // @Input() slideValue: number;    
+    // elementRef: ElementRef;
+    // visibility = 'hidden';
+    // @Input() index: number;
     
-    constructor(@Inject(ElementRef) elementRef: ElementRef) {
-      this.elementRef = elementRef;
-    }
+    // constructor(@Inject(ElementRef) elementRef: ElementRef) {
+    //   this.elementRef = elementRef;
+    // }
 
-    updSlider() {
-      jQuery(this.elementRef.nativeElement).find("#slider")
-      .slider("option", "value", this.slideValue);      
-    }
+    // updSlider() {
+    //   jQuery(this.elementRef.nativeElement).find("#slider")
+    //   .slider("option", "value", this.slideValue);      
+    // }
 
-    ngAfterViewInit() { 
-      jQuery(this.elementRef.nativeElement).find("#slider").slider({
-        range: "min",
-        orientation: "vertical",
-        min: 0,
-        max: 300,
-        animate: true,
-        step: 1,
-        value: this.slideValue,
-        slide: ( event, ui ) => {
-          this.slideValue = ui.value;
-          this.visibility = "visible";
-          jQuery(this.elementRef.nativeElement).find(".ui-slider-range").css({"background": "#FFFFcc"});
-        }
-      });
-    }
+    // ngAfterViewInit() { 
+    //   jQuery(this.elementRef.nativeElement).find("#slider").slider({
+    //     range: "min",
+    //     min: 0,
+    //     max: 300,
+    //     animate: true,
+    //     step: 1,
+    //     value: this.slideValue,
+    //     slide: ( event, ui ) => {
+    //       this.slideValue = ui.value;
+    //       this.visibility = "visible";
+    //       jQuery(this.elementRef.nativeElement).find(".ui-slider-range").css({"background": "#FFFFcc"});
+    //     }
+    //   });
+    // }
 }
